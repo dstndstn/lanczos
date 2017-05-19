af47d57cf09182f9182301b394e9557c3d4cf7c3
Dustin Lang <dstndstn@gmail.com>
2017-05-19 12:12:42 -0400

    fourier space too

--- lan.py
+++ lan.py
@@ -13,13 +13,16 @@ of the kernel and offsetting by that amount.
 '''
 ps = PlotSequence('lan')
 
-subsampling = 100
+subsampling = 256
 
 xx = np.arange(-4., 3+1.).astype(np.float32)
+xx += 0.5 * (1./subsampling)
 
 lanczos_kernel = np.zeros((len(xx) * subsampling))
 fixed_kernel = np.zeros((len(xx) * subsampling))
 fixed2_kernel = np.zeros((len(xx) * subsampling))
+
+subx = np.zeros((len(xx) * subsampling))
 
 fixed_x = np.zeros(subsampling)
 fixed_x2 = np.zeros(subsampling)
@@ -35,6 +38,8 @@ for i,dx in enumerate(DX):
     assert(rtn == 0)
     lanczos_kernel[i::subsampling] = L
 
+    subx[i::subsampling] = xx + dx
+    
     print('dx', dx)
     Lsum = L.sum()
     #print('sum', L.sum())
@@ -61,21 +66,70 @@ for i,dx in enumerate(DX):
     centroid = -np.sum(L * xx) / Lsum
     print('centroid', centroid, 'vs', dx)
     cen2[i] = centroid
+
+print('subx', subx.min(), subx.max())
     
 plt.clf()
-plt.plot(lanczos_kernel, 'b-')
-plt.plot(fixed_kernel, 'r-')
-plt.plot(fixed2_kernel, 'g-')
+plt.plot(subx, lanczos_kernel, 'b-', lw=3, alpha=0.5, label='Lanczos')
+#plt.plot(fixed_kernel, 'r-')
+plt.plot(subx, fixed2_kernel, 'r-', label='"fixed(2)"')
+plt.xlim(-3.5, 3.5)
+plt.legend()
 ps.savefig()
     
 plt.clf()
 plt.plot(DX, cen0, 'b-')
-plt.plot(DX, cen1, 'r-')
-plt.plot(DX, cen2, 'g-')
+plt.plot(DX, cen1, 'g-')
+plt.plot(DX, cen2, 'r-')
 ps.savefig()
 
 plt.clf()
-plt.plot(DX, cen0 - DX, 'b-')
-plt.plot(DX, cen1 - DX, 'r-')
-plt.plot(DX, cen2 - DX, 'g-')
+plt.plot(DX, cen0 - DX, 'b-', label='Lanczos-3')
+plt.plot(DX, cen1 - DX, 'g-', label='"fixed(1)"')
+plt.plot(DX, cen2 - DX, 'r-', label='"fixed(2)"')
+plt.xlabel('subpixel offset (pixels)')
+plt.ylabel('centroid error (pixels)')
+plt.legend()
 ps.savefig()
+
+plt.clf()
+plt.plot(DX, fixed_x - DX, 'g-')
+plt.plot(DX, fixed_x2 - DX, 'r-')
+ps.savefig()
+
+F0 = np.fft.rfft(lanczos_kernel)
+F1 = np.fft.rfft(fixed_kernel)
+F2 = np.fft.rfft(fixed2_kernel)
+
+print(len(lanczos_kernel))
+
+plt.clf()
+plt.subplot(2,1,1)
+plt.plot(F0.real, 'b-')
+plt.plot(F1.real, 'g-')
+plt.plot(F2.real, 'r-')
+plt.xscale('log')
+plt.ylabel('Fourier transform (real)')
+plt.subplot(2,1,2)
+plt.plot(F0.imag, 'b-')
+plt.plot(F1.imag, 'g-')
+plt.plot(F2.imag, 'r-')
+plt.xscale('log')
+plt.ylabel('Fourier transform (imag)')
+ps.savefig()
+
+mag0 = np.hypot(F0.real, F0.imag)
+mag1 = np.hypot(F1.real, F1.imag)
+mag2 = np.hypot(F2.real, F2.imag)
+
+plt.clf()
+plt.plot(mag0, 'b-', label='Lanczos-3')
+plt.plot(mag1, 'g-')
+plt.plot(mag2, 'r-', label='"fixed"(2)')
+plt.xscale('log')
+plt.yscale('log')
+plt.ylabel('abs Fourier transform')
+plt.xlabel('frequency')
+plt.legend()
+ps.savefig()
+

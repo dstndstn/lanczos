a214e3002066fdb8048f31ae08328dacb9857b8c
Dustin Lang <dstndstn@gmail.com>
2017-05-23 13:50:23 -0400

    check what SourceExtractor (and the Tractor) do on Lanczos-shifted images

--- swarp2.py
+++ swarp2.py
@@ -3,14 +3,16 @@ import os
 import numpy as np
 import fitsio
 from astrometry.util.util import *
+from astrometry.util.fits import fits_table
 from astrometry.util.file import *
 from astrometry.util.plotutils import *
 import pylab as plt
+from tractor import *
 
 '''
-Try shifting a source Gaussian through a pixel (one dimension)
-and then also resampling by a fraction of a pixel, and measuring
-the dcen3x3 centroid.
+Try shifting a source Gaussian through a pixel (in one dimension) and
+then also resampling by a fraction of a pixel (in the same dimension),
+and measuring the dcen3x3 centroid.
 '''
 
 ps = PlotSequence('dx')
@@ -23,11 +25,17 @@ N1 = N2 = 25
 
 ocen  = np.zeros((N1,N2))
 odcen = np.zeros((N1,N2))
+osecen = np.zeros((N1,N2))
+osewcen = np.zeros((N1,N2))
+otcen = np.zeros((N1,N2))
 
 ic = np.zeros((N1,N2))
 
 icen = np.zeros(N1)
 idcen = np.zeros(N1)
+isecen = np.zeros(N1)
+isewcen = np.zeros(N1)
+itcen = np.zeros(N1)
 
 OX = np.linspace(0, 2, N1)
 DX = np.linspace(0, 2, N2)
@@ -35,7 +43,7 @@ DX = np.linspace(0, 2, N2)
 cx,cy = w//2, h//2
 print('cx', cx)
 for ii,ox in enumerate(OX):
-
+    print('Gaussian centered at', ox, ', ', ii+1, 'of', len(OX))
     sig = 3.
     img = np.exp(-0.5 * ((xx - (cx+ox))**2 + (yy - cy)**2) / sig**2)
 
@@ -54,6 +62,28 @@ for ii,ox in enumerate(OX):
                           three[6], three[7], three[8])
     idcen[ii] = x + px - 1
 
+    fitsio.write('input1.fits', img, clobber=True)
+
+    # run source extractor on the input image
+    cmd = 'sex -c se.conf input1.fits'
+    rtn = os.system(cmd)
+    #print('Return:', rtn)
+    # -> se.fits
+    assert(rtn == 0)
+    T = fits_table('se.fits')
+    assert(len(T) == 1)
+    isecen[ii] = T.x_image[0] - 1.
+    isewcen[ii] = T.xwin_image[0] - 1.
+
+    # run the tractor on the input image
+    tim = Image(data=img, inverr=np.ones_like(img),
+                psf=NCircularGaussianPSF([sig],[1.]))
+    src = PointSource(PixPos(cx, cy), Flux(1.))
+    tractor = Tractor([tim], [src])
+    tractor.freezeParam('images')
+    tractor.optimize_loop()
+    itcen[ii] = src.pos.x
+    
     dy = 0.
     for jj,dx in enumerate(DX):
         hdr = fitsio.FITSHDR()
@@ -90,9 +120,31 @@ for ii,ox in enumerate(OX):
                               three[6], three[7], three[8])
         odcen[ii,jj] = x + px - 1
 
+        # run source extractor on the output (resampled) image
+        cmd = 'sex -c se.conf %s' % outfn
+        rtn = os.system(cmd)
+        assert(rtn == 0)
+        T = fits_table('se.fits')
+        assert(len(T) == 1)
+        osecen [ii,jj] = T.x_image   [0] - 1.
+        osewcen[ii,jj] = T.xwin_image[0] - 1.
+
+        # run the tractor on the output (resampled) image.  We re-use
+        # the data structures from above, AND the position from the
+        # previous fit, but that's okay because the 'coimg' pixels are
+        # different each time, and the initialization doesn't matter
+        # much.
+        tim.data = coimg
+        tractor.optimize_loop()
+        otcen[ii,jj] = src.pos.x
+        
+
 plt.clf()
 plt.plot(OX, icen, 'b-', label='Input centroid')
 plt.plot(OX, idcen, 'r-', label='Input dcen3x3')
+plt.plot(OX, isecen, 'g-', label='Input SE x_image')
+plt.plot(OX, isewcen, 'm-', label='Input SE xwin_image')
+plt.plot(OX, itcen, 'c-', label='Input tractor pos')
 plt.xlabel('Input subpixel center')
 plt.ylabel('Input measured centroid')
 plt.legend(loc='upper left')
@@ -101,6 +153,9 @@ ps.savefig()
 plt.clf()
 plt.plot(OX, icen-(OX+cx), 'b-', label='Input centroid')
 plt.plot(OX, idcen-(OX+cx), 'r-', label='Input dcen3x3')
+plt.plot(OX, isecen-(OX+cx), 'g-', label='Input SE x_image')
+plt.plot(OX, isewcen-(OX+cx), 'm-', label='Input SE xwin_image')
+plt.plot(OX, itcen-(OX+cx), 'm-', label='Input tractor pos')
 plt.xlabel('Input subpixel center')
 plt.ylabel('Input measured centroid error')
 plt.legend(loc='upper left')
@@ -116,37 +171,19 @@ plt.title('Lanczos-shifted centroid')
 plt.colorbar()
 ps.savefig()
 
-plt.clf()
-plt.imshow(ocen - ic, interpolation='nearest', origin='lower',
-           extent=[DX.min(), DX.max(), OX.min(), OX.max()],
-           cmap='RdBu')
-print('ic', ic.min(), ic.max())
-plt.xlabel('subpixel shift')
-plt.ylabel('input subpixel center')
-plt.title('Lanczos-shifted centroid error')
-plt.colorbar()
-ps.savefig()
-
-plt.clf()
-plt.imshow(odcen, interpolation='nearest', origin='lower',
-           extent=[DX.min(), DX.max(), OX.min(), OX.max()])
-print('odcen', odcen.min(), odcen.max())
-plt.xlabel('subpixel shift')
-plt.ylabel('input subpixel center')
-plt.title('Lanczos-shifted dcen3x3 result')
-plt.colorbar()
-ps.savefig()
-
-plt.clf()
-plt.imshow(odcen - ic, interpolation='nearest', origin='lower',
-           extent=[DX.min(), DX.max(), OX.min(), OX.max()],
-           cmap='RdBu')
-plt.xlabel('subpixel shift')
-plt.ylabel('input subpixel center')
-plt.title('Lanczos-shifted dcen3x3 error')
-plt.colorbar()
-ps.savefig()
-
+for name,img in [('centroid', ocen), ('dcen3x3', odcen),
+                 ('SE x_image', osecen), ('SE xwin_image', osewcen),
+                 ('Tractor (w/ correct PSF)', otcen)]:
+    plt.clf()
+    absmax = np.max(np.abs(img - ic))
+    plt.imshow(img - ic, interpolation='nearest', origin='lower',
+               extent=[DX.min(), DX.max(), OX.min(), OX.max()],
+               cmap='RdBu', vmin=-absmax, vmax=absmax)
+    plt.xlabel('subpixel shift')
+    plt.ylabel('input subpixel center')
+    plt.title('Error in Lanczos-shifted center: %s' % name)
+    plt.colorbar()
+    ps.savefig()
 
 plt.clf()
 plt.imshow(odcen - ocen, interpolation='nearest', origin='lower',
